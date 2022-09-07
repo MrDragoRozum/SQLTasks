5880dc7b2c4a9e4d9ae18830b79674a66ef403cd
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2022-09-07 15:51:05 +0300

    Решил задачу

---
+++ Сортировка полученных данных/Task1.sql
@@ -0,0 +1,7 @@
+--Напишите инструкцию SQL, которая извлекает
+--имена всех клиентов (cust_name) из таблицы
+--Customers и сортирует результаты по убыванию.
+
+SELECT cust_name
+FROM Customers
+ORDER BY 1 DESC;
