af2b73654474be680e9c30d06a4711b59040db15
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2022-09-16 18:07:34 +0300

    Create Task3.sql

---
+++ Подзапросы/Task3.sql
@@ -0,0 +1,14 @@
+--Давайте усложним задачу. Модифицируйте преды­
+--дущую инструкцию, чтобы она возвращала адрес
+--электронной почты клиента (поле cust_email таблицы Customers)
+--для любого клиента, купив­шего товар с идентификатором 'BR01'.
+
+SELECT cust_email 
+FROM Customers
+WHERE cust_id IN(
+    SELECT cust_id
+    FROM Orders
+    WHERE order_num IN(
+        SELECT order_num
+        FROM OrderItems
+        prod_id = 'BR01'));
